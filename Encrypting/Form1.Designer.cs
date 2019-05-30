be2d7e40092f2265339821fd6a756b85bc88f0b5
Razvan <razvan7birt@gmail.com>
2019-05-30 15:25:44 +0100

    First Commit

---
+++ Encrypting/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Encrypting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxArrayStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(435, 12);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(94, 21);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Select Folder";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(330, 20);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(12, 53);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxConsole.Size = new System.Drawing.Size(682, 334);
+            this.textBoxConsole.TabIndex = 3;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(535, 12);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(159, 21);
+            this.buttonEncrypt.TabIndex = 4;
+            this.buttonEncrypt.Text = "Start Encrypting Files";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxArrayStart
+            // 
+            this.textBoxArrayStart.Location = new System.Drawing.Point(13, 414);
+            this.textBoxArrayStart.Name = "textBoxArrayStart";
+            this.textBoxArrayStart.Size = new System.Drawing.Size(100, 20);
+            this.textBoxArrayStart.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 451);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxArrayStart);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "QuickEncryptor";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxArrayStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Encrypting/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using WindowsInput;
+using WindowsInput.Native;
+namespace Encrypting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path;
+        static string[] filePaths;
+        static string projectDirectory = Environment.CurrentDirectory;
+        static string unviewableDirectory = projectDirectory + @"\Unviewable\UnviewablePlus.exe";
+        static string templateDirectory = projectDirectory + @"\Unviewable\TEMPLATE.XLSB";
+        static string templateDirectoryOriginal = projectDirectory + @"\TEMPLATE.XLSB";
+        string nameFile = "";
+
+        Process p = new Process();
+        
+        string[] currentDirectories;
+        string[] filePaths_xlsm;
+        string[] filePaths_xlsb;
+
+        static DateTime currentT = DateTime.Now;
+        static string currentTime = currentT.ToString("ddMMyy-HHmm");
+        
+        static string logFilePath = @".\log.txt";
+
+        InputSimulator sim = new InputSimulator();
+        StreamWriter sw = new StreamWriter(logFilePath, true);
+
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = @"S:\PARIS-VAT\VATSystems_PRODUCTION\PROCESS_ACTIVITY\CLIENTS";       
+            // open folder browser to choose path for encryption 
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+            path = folderBrowserDialog1.SelectedPath;
+            textBoxPath.Text = path;
+            }
+        
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            //create text file for logs
+            path = textBoxPath.Text;
+            // get main directories for then encrypt files in side at a time
+            currentDirectories = Directory.GetDirectories(path);
+
+            // open and focus on unviewable window         
+            p = Process.Start(unviewableDirectory);
+
+            IntPtr h = p.MainWindowHandle;
+            SetForegroundWindow(h);
+
+            System.Threading.Thread.Sleep(3000);
+
+            copyFile(templateDirectoryOriginal, Directory.GetCurrentDirectory() + @"\Unviewable");
+            textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + "----------------------------------------------------------";
+            sw.WriteLine("----------------------------------------------------------");
+
+            // starting process of encryption
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("^o");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait(templateDirectory);
+            System.Threading.Thread.Sleep(3000);
+
+            // run first cicle
+            SendKeys.SendWait("{ENTER}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{TAB}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{UP}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{TAB}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait(" ");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{ENTER}");
+            System.Threading.Thread.Sleep(3000);
+            SendKeys.SendWait("{ENTER}");
+
+            System.Threading.Thread.Sleep(3000);
+
+            int x = int.Parse(textBoxArrayStart.Text);
+
+            for (int i = x; i < currentDirectories.Length; i++)
+            {
+                label1.Text = i.ToString();
+                encrypt_files(currentDirectories[i]);              
+            }
+            System.Threading.Thread.Sleep(500);
+            sw.Close();
+            p.Kill();
+        }
+
+        private void encrypt_files(string subPath)
+        {
+            // find both excel extensions
+            filePaths_xlsm = Directory.GetFiles(subPath, "*.xlsm", SearchOption.AllDirectories);
+            filePaths_xlsb = Directory.GetFiles(subPath, "*.xlsb", SearchOption.AllDirectories);
+            
+            // combine both arrays into one for then have a complete one to start encrypting on by one
+            Array.Resize<string>(ref filePaths, filePaths_xlsm.Length + filePaths_xlsb.Length);
+            filePaths_xlsm.CopyTo(filePaths, 0);
+            filePaths_xlsb.CopyTo(filePaths, filePaths_xlsm.Length);          
+
+            if (filePaths.Length != 0)
+            {
+                textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + subPath + ": " + filePaths.Length + " files to encrypt";
+                sw.WriteLine(subPath + ": " + filePaths.Length + " files to encrypt");
+            }
+            else
+            {
+                textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + subPath + ": 0 files to encrypt";
+                sw.WriteLine(subPath + ": 0 files to encrypt");
+            }
+
+            int nFiles = 0;
+            if (filePaths.Length != 0)
+            {
+                System.Threading.Thread.Sleep(1500);
+                // run second and final cicle, because its different while unviewable its still open saves some stuff done to it
+                for (int i = 0; i < filePaths.Length; i++)
+                {                 
+                    nFiles++;
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("^o");
+                    System.Threading.Thread.Sleep(1000);
+                    // new way to send text to the textbox of unviewable openFileDialog
+                    sim.Keyboard.TextEntry(filePaths[i]);
+
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("{ENTER}");
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("{TAB}{TAB}");
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait(" ");
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}");
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("{ENTER}");
+                    System.Threading.Thread.Sleep(1000);
+                    SendKeys.SendWait("{ENTER}");
+
+                    currentT = DateTime.Now;
+                    currentTime = currentT.ToString("ddMMyy-HHmm");
+                    textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + currentT + " - " + filePaths[i] + " ||Encryption Done";               
+                    sw.WriteLine(currentT + " - " + filePaths[i] + " ||Encryption Done");
+                }
+                textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + nFiles + " files were Encrypted";
+                sw.WriteLine(nFiles + " files were Encrypted");
+            }
+        }
+
+        private void copyFile(string FOri, string FDest)
+        {
+            if (File.Exists(FOri) == false)
+                MessageBox.Show("Error file does not exist");
+            else
+            {
+                try
+                {
+                    nameFile = Path.GetFileName(FOri);
+                    FDest = FDest + "\\" + nameFile;
+                    File.Copy(FOri, FDest,true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void readFile(string file, int numberOfLines)
+        {
+
+            StreamReader readFile = new StreamReader(file);
+            string[] arrayOfLines = new string[numberOfLines];
+
+            for( int i = 1; i < numberOfLines; i++)
+            {
+                arrayOfLines[i] = readFile.ReadLine();
+                //textBoxConsole.Text = textBoxConsole.Text + System.Environment.NewLine + arrayOfLines[i];
+            }      
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
